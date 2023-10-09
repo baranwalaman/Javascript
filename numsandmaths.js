88022fa8c7c59ef788d4eabcbaa7bbadc6e3d52e
Aman Baranwal <amanbaranwal2017@gmail.com>
2023-10-09 21:24:55 +0530

    numsandmaths

--- numsandmaths.js
+++ numsandmaths.js
@@ -1,4 +1,12 @@
 let score = 300
-console.log(score);
-const newScore = new Number(500)
-console.log(newScore);
+// console.log(score)
+const newScore = new Number(500.2)
+// console.log(newScore)
+// console.log(newScore.toPrecision(3))
+let num = 2000003
+// console.log(num.toLocaleString('en-IN'))
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+let myDate = new Date()
+console.log(myDate.toString());
