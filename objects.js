c3478cb80e9339c7e4eda34a9392f38e4d28dca0
Aman Baranwal <amanbaranwal2017@gmail.com>
2023-10-10 23:58:46 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,30 @@
+//literals
+const JSuser = {
+    name: "Aman Baranwal",
+    class: "Btechcse",
+    age: 22,
+    address: "Bhadohi"
+}
+// console.log(JSuser.name);
+// console.log(JSuser["name"]);
+
+//--------------------------------------------------------------------------------
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+// console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+// console.log(returnedTarget === target);
+// Expected output: true
+
+// -------------------------------------------------------------------------------
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
+
