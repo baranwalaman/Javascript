e646c336dd14edffe47c049019beb9a41c281de2
Aman Baranwal <amanbaranwal2017@gmail.com>
2023-10-11 23:31:37 +0530

    functions

---
+++ function.js
@@ -0,0 +1,73 @@
+// function sumOfNumbers(num1, num2){
+//     console.log(num1+num2)
+// }
+// result = sumOfNumbers(5, 2)
+// console.log("Result : ",result);
+
+
+
+
+// function sumOfNumbers(num1, num2){
+//     result = num1+num2
+//     return result
+// }
+// result = sumOfNumbers(5, 2)
+// console.log("Result : ",result);
+
+
+
+
+
+// function sumOfNumbers(num1, num2){
+//     return num1+num2
+// }
+// result = sumOfNumbers(5, 2)
+// console.log("Result : ",result);
+
+
+
+
+
+// function isLoggedIn(username = "No one"){
+//     console.log((`${username} just logged in`));
+// }
+// isLoggedIn("Aman")
+
+
+
+
+
+// function calculateCartPrice(val1, val2...num){
+//     return num
+// }
+// console.log(calculateCartPrice(500, 1000, 799, 450));
+
+
+
+
+
+// const user = {
+//     name : "Aman",
+//     id : 356
+// }
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.name} and id is ${anyobject.id}`);
+// }
+
+// handleObject(user)
+
+// handleObject({
+//     name : "Anyuser",
+//     id : 123
+// })
+
+
+
+
+
+const myArr = [2, 5, 7, 12]
+function getSecondValue(getarray){
+    console.log(getarray[1]);
+}
+// getSecondValue(myArr)
+getSecondValue([2, 5, 7, 12])
